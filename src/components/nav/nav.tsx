600449fd38eed226a0bfdba373669691f9aff15c
Alex Khitry <khitrii03@gmail.com>
2023-06-06 21:25:29 +0300

    feat: add adaptivity to header

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './nav.scss';
 
 const Nav = () => {
+  const maxwidth480 = '(max-width: 480px)';
+  const [isMaxWidth480, setIsMaxWidth480] = useState(window.matchMedia(maxwidth480).matches);
+  useEffect(() => {
+    window.matchMedia(maxwidth480).addEventListener('change', (e) => setIsMaxWidth480(e.matches));
+  }, []);
   return (
     <nav className="nav">
-      <p className="nav__link">Home</p>
-      <p className="nav__link">Projects</p>
-      <p className="nav__link">Skills</p>
+      {isMaxWidth480 ? (
+        <p className="nav__menu">Menu</p>
+      ) : (
+        <>
+          <p className="nav__link">Home</p>
+          <p className="nav__link">Projects</p>
+          <p className="nav__link">Skills</p>
+        </>
+      )}
     </nav>
   );
 };
