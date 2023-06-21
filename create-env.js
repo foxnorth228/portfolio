4755ef3bbc4fb3e1c13e041bda597d355ed193b2
Alex Khitry <khitrii03@gmail.com>
2023-06-22 00:49:48 +0300

    fix: fix create-env from ES module to require

--- create-env.js
+++ create-env.js
@@ -1,4 +1,4 @@
-import { writeFileSync } from 'fs';
-writeFileSync('./.env', `SERVISE_ID=${process.env.SERVISE_ID}\n`);
-writeFileSync('./.env', `TEMPLATE_ID=${process.env.TEMPLATE_ID}\n`);
-writeFileSync('./.env', `PUBLIC_API_KEY=${process.env.PUBLIC_API_KEY}\n`);
+const fs = require('fs')
+fs.writeFileSync('./.env', `SERVISE_ID=${process.env.SERVISE_ID}\n`);
+fs.writeFileSync('./.env', `TEMPLATE_ID=${process.env.TEMPLATE_ID}\n`);
+fs.writeFileSync('./.env', `PUBLIC_API_KEY=${process.env.PUBLIC_API_KEY}\n`);
